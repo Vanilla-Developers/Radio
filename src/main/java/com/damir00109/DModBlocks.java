4bf568df9f66318c9bf6e6e49d0e4785e9a44a28
inter <internet00109@gmail.com>
2025-05-01 23:27:17 +0300

    Missing Item FIX #1

--- src/main/java/com/damir00109/DModBlocks.java
+++ src/main/java/com/damir00109/DModBlocks.java
@@ -1,78 +1,95 @@
 package com.damir00109;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.EnumProperty;
-import net.minecraft.state.property.IntProperty;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Direction;
-import net.minecraft.item.ItemPlacementContext;
 
 public class DModBlocks {
-    public static final EnumProperty<Direction> FACING = EnumProperty.of("facing", Direction.class);
+    // Горизонтальные направления
+    public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
 
-
-    public static final Block RADIO = registerBlock("radio", new Radio.RadioBlock(Block.Settings.create()
-            .mapColor(MapColor.STONE_GRAY)
-            .strength(1.5f)
-            .pistonBehavior(PistonBehavior.BLOCK)
-            .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VanillaDamir00109.MOD_ID, "radio")))
-    ));
+    // Регистрация блоков статически
+    public static final Block RADIO = registerBlock(
+            "radio",
+            settings -> new Radio.RadioBlock(settings),
+            Block.Settings.create()
+                    .mapColor(MapColor.STONE_GRAY)
+                    .strength(1.5f)
+                    .pistonBehavior(PistonBehavior.BLOCK)
+    );
 
     public static final Block BURNT_RADIO = registerBlock(
             "burnt_radio",
-            new BurntRadioBlock(Block.Settings.create()
-                    .pistonBehavior(PistonBehavior.BLOCK)
+            settings -> new BurntRadioBlock(settings),
+            Block.Settings.create()
                     .mapColor(MapColor.ORANGE)
                     .strength(3.0f)
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK,
-                            Identifier.of(VanillaDamir00109.MOD_ID, "burnt_radio")))
-            )
+                    .pistonBehavior(PistonBehavior.BLOCK)
     );
 
-    private static Block registerBlock(String name, Block block) {
+    // Универсальный метод регистрации, устанавливает registryKey до создания блока и Item
+    private static Block registerBlock(
+            String name,
+            java.util.function.Function<Block.Settings, Block> factory,
+            Block.Settings baseSettings
+    ) {
         Identifier id = Identifier.of(VanillaDamir00109.MOD_ID, name);
+        // Регистрируем блок со своим registryKey
+        RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, id);
+        baseSettings = baseSettings.registryKey(blockKey);
+        Block block = factory.apply(baseSettings);
         Registry.register(Registries.BLOCK, id, block);
-        Registry.register(Registries.ITEM, id, new BlockItem(block, new Item.Settings()
-                .registryKey(RegistryKey.of(RegistryKeys.ITEM, id))
-        ));
+
+        // Регистрируем Item для блока с корректным registryKey
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings itemSettings = new Item.Settings().registryKey(itemKey);
+        Registry.register(
+                Registries.ITEM,
+                id,
+                new BlockItem(block, itemSettings)
+        );
+
         return block;
     }
 
     public static void registerModBlocks() {
         VanillaDamir00109.LOGGER.info("Registering Mod Blocks for " + VanillaDamir00109.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
-            entries.add(BURNT_RADIO);
-            entries.add(RADIO);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE)
+                .register(entries -> {
+                    entries.add(BURNT_RADIO);
+                    entries.add(RADIO);
+                });
     }
 
+    // Класс блока BurntRadio
     public static class BurntRadioBlock extends Block {
         public BurntRadioBlock(Settings settings) {
             super(settings);
-            this.setDefaultState(this.stateManager.getDefaultState()
-                    .with(FACING, Direction.NORTH));
+            setDefaultState(getStateManager().getDefaultState().with(FACING, Direction.NORTH));
         }
 
         @Override
-        protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        protected void appendProperties(StateManager.Builder<Block, net.minecraft.block.BlockState> builder) {
             builder.add(FACING);
         }
 
         @Override
-        public BlockState getPlacementState(ItemPlacementContext ctx) {
-            return this.getDefaultState()
-                    .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        public net.minecraft.block.BlockState getPlacementState(ItemPlacementContext ctx) {
+            return getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
         }
     }
 }
