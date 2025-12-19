64b54037757dc60f601e5dbc35c3553784ec7813
Damir00109.github.io <99585754+Damir00109@users.noreply.github.com>
2025-12-19 14:04:33 +0300

    Refactor onInitializeClient for property registration
    Updated the onInitializeClient method to register a new property with a placeholder registry.

--- src/main/java/ru/dimaskama/radio/client/RadioModClient.java
+++ src/main/java/ru/dimaskama/radio/client/RadioModClient.java
@@ -1,11 +1,20 @@
 package ru.dimaskama.radio.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.class_10459;
+import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+
 import ru.dimaskama.radio.RadioMod;
 
 public class RadioModClient implements ClientModInitializer {
-	public void onInitializeClient() {
-		class_10459.field_55372.method_65325(RadioMod.id("radio_destroyed"), RadioDestroyedProperty.CODEC);
-	}
+    @Override
+    public void onInitializeClient() {
+        // TODO: Замените PLACEHOLDER_REGISTRY на точный реестр (например, BuiltinRegistries.SOME_REGISTRY или Registries.SOME_KEY)
+        // Вариант A: если это встроенный реестр (builtin):
+        BuiltinRegistries.register(BuiltinRegistries.PLACEHOLDER_REGISTRY, RadioMod.id("radio_destroyed"), RadioDestroyedProperty.CODEC);
+
+        // Вариант B: если нужно использовать общий Registry API:
+        // Registry.register(Registries.PLACEHOLDER_REGISTRY, RadioMod.id("radio_destroyed"), RadioDestroyedProperty.CODEC);
+    }
 }
