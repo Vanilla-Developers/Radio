611ed09b76a7e923c5b6e85cb8d4ccb7094fe0e2
Damir00109 <internet00109@gmail.com>
2025-12-19 17:43:36 +0300

    fix

--- src/main/java/ru/dimaskama/radio/RadioMod.java
+++ src/main/java/ru/dimaskama/radio/RadioMod.java
@@ -6,7 +6,6 @@ import java.nio.file.Path;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.EndWorldTick;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.server.world.ServerWorld;
@@ -21,42 +20,43 @@ import ru.dimaskama.radio.extend.ServerWorldExtend;
 import ru.dimaskama.radio.item.ModItems;
 
 public class RadioMod implements ModInitializer {
-	public static final String MOD_ID = "radio";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final JsonConfig<RadioConfig> CONFIG = new JsonConfig<>("config/radio/config.json", RadioConfig.CODEC, RadioConfig::new);
-	public static final ModContainer MOD_CONTAINER = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
-	public static final Path CONFIG_DIR = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID);
-	public static final Path SOUNDS_DIR = CONFIG_DIR.resolve("sounds");
-	public static final String COMMAND_PERMISSION = "radio.command";
+    public static final String MOD_ID = "radio";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final JsonConfig<RadioConfig> CONFIG = new JsonConfig<>("config/radio/config.json", RadioConfig.CODEC, RadioConfig::new);
+    public static final ModContainer MOD_CONTAINER = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
+    public static final Path CONFIG_DIR = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID);
+    public static final Path SOUNDS_DIR = CONFIG_DIR.resolve("sounds");
+    public static final String COMMAND_PERMISSION = "radio.command";
 
-	@Override
-	public void onInitialize() {
-		createDir(CONFIG_DIR);
-		createDir(SOUNDS_DIR);
-		CONFIG.loadOrCreate();
-		ModBlocks.init();
-		ModBlockEntities.init();
-		ModItems.init();
-		CommandRegistrationCallback.EVENT.register(new RadioCommand());
-		ServerTickEvents.END_WORLD_TICK.register((ServerWorld world) -> {
-			if (world.getServer().isRunning()) {
-				WorldRadioManager radioManager = ((ServerWorldExtend) world).radio_getRadioManager();
-				if (radioManager != null) {
-					radioManager.tick();
-				}
-			}
-		});
-	}
+    @Override
+    public void onInitialize() {
+        createDir(CONFIG_DIR);
+        createDir(SOUNDS_DIR);
+        CONFIG.loadOrCreate();
+        ModBlocks.init();
+        ModBlockEntities.init();
+        ModItems.init();
+        CommandRegistrationCallback.EVENT.register(new RadioCommand());
+        ServerTickEvents.END_WORLD_TICK.register((ServerWorld world) -> {
+            if (world.getServer().isRunning()) {
+                WorldRadioManager radioManager = ((ServerWorldExtend) world).radio_getRadioManager();
+                if (radioManager != null) {
+                    radioManager.tick();
+                }
+            }
+        });
+    }
 
-	private static void createDir(Path path) {
-		try {
-			Files.createDirectories(path);
-		} catch (IOException var2) {
-			throw new RuntimeException("Failed to create directory " + path, var2);
-		}
-	}
+    private static void createDir(Path path) {
+        try {
+            Files.createDirectories(path);
+        } catch (IOException var2) {
+            throw new RuntimeException("Failed to create directory " + path, var2);
+        }
+    }
 
-	public static Identifier id(String path) {
-		return new Identifier(MOD_ID, path);
-	}
+    public static Identifier id(String path) {
+        // Исправление: new Identifier() -> Identifier.of()
+        return Identifier.of(MOD_ID, path);
+    }
 }
