3dea66d8d6414dddc20a82fef78eb50a6ecd29e2
Damir00109 <internet00109@gmail.com>
2025-12-19 14:43:06 +0300

    item

--- src/main/java/ru/dimaskama/radio/item/RadioItem.java
+++ src/main/java/ru/dimaskama/radio/item/RadioItem.java
@@ -1,21 +1,27 @@
 package ru.dimaskama.radio.item;
 
-import net.minecraft.class_1747;
-import net.minecraft.class_1799;
-import net.minecraft.class_2248;
-import net.minecraft.class_2561;
-import net.minecraft.class_1792.class_1793;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.text.Text;
+import net.minecraft.item.Item.Settings;
+
 import ru.dimaskama.radio.RadioState;
 import ru.dimaskama.radio.item.ModItems.DataComponents;
 
-public class RadioItem extends class_1747 {
-	private static final class_2561 DESTROYED_TEXT = class_2561.method_43471("block.radio.radio.destroyed");
+public class RadioItem extends BlockItem {
 
-	public RadioItem(class_2248 block, class_1793 settings) {
-		super(block, settings);
-	}
+    private static final Text DESTROYED_TEXT =
+            Text.translatable("block.radio.radio.destroyed");
 
-	public class_2561 method_7864(class_1799 stack) {
-		return stack.method_58694(DataComponents.RADIO_STATE) != RadioState.DESTROYED ? super.method_7864(stack) : DESTROYED_TEXT;
-	}
+    public RadioItem(Block block, Settings settings) {
+        super(block, settings);
+    }
+
+    @Override
+    public Text getName(ItemStack stack) {
+        return stack.get(DataComponents.RADIO_STATE) != RadioState.DESTROYED
+                ? super.getName(stack)
+                : DESTROYED_TEXT;
+    }
 }
