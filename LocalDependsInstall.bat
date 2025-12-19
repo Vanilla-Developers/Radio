5a93bd9499aa77f2f4384cd1362bb5c604eb7240
Damir00109 <internet00109@gmail.com>
2025-12-19 13:23:00 +0300

    DEL

--- LocalDependsInstall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Run the project dependency installation script
-git clone https://github.com/QuiltServerTools/Ledger.git
-cd Ledger
-echo Cloning of dependency repositories completed
-gradlew publishToMavenLocal
-echo Installation successful
-pause
