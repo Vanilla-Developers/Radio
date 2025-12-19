f81245bb71ce20cd8114c6a0247d080e3a7fba4f
Damir00109 <internet00109@gmail.com>
2025-12-19 14:49:41 +0300

    mixin

--- src/main/java/ru/dimaskama/radio/mixin/voicechat/LocationalAudioChannelImplMixin.java
+++ src/main/java/ru/dimaskama/radio/mixin/voicechat/LocationalAudioChannelImplMixin.java
@@ -2,7 +2,7 @@ package ru.dimaskama.radio.mixin.voicechat;
 
 import de.maxhenkel.voicechat.api.ServerLevel;
 import de.maxhenkel.voicechat.voice.common.LocationSoundPacket;
-import net.minecraft.class_3218;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,22 +13,27 @@ import ru.dimaskama.radio.VoiceIntegration;
 
 @Pseudo
 @Mixin(
-	targets = {"de.maxhenkel.voicechat.plugins.impl.audiochannel.LocationalAudioChannelImpl"}
+        targets = {"de.maxhenkel.voicechat.plugins.impl.audiochannel.LocationalAudioChannelImpl"}
 )
 abstract class LocationalAudioChannelImplMixin {
-	@Shadow(
-		remap = false
-	)
-	protected ServerLevel level;
 
-	@Inject(
-		method = {"broadcast"},
-		at = {@At("HEAD")},
-		remap = false
-	)
-	private void broadcastHead(LocationSoundPacket packet, CallbackInfo ci) {
-		if (this.level.getServerLevel() instanceof class_3218 world) {
-			VoiceIntegration.onPluginLocationPacket(world, packet.getLocation(), packet.getChannelId(), packet.getData(), packet.getDistance());
-		}
-	}
+    @Shadow(remap = false)
+    protected ServerLevel level;
+
+    @Inject(
+            method = {"broadcast"},
+            at = @At("HEAD"),
+            remap = false
+    )
+    private void broadcastHead(LocationSoundPacket packet, CallbackInfo ci) {
+        if (this.level.getServerLevel() instanceof ServerWorld world) {
+            VoiceIntegration.onPluginLocationPacket(
+                    world,
+                    packet.getLocation(),
+                    packet.getChannelId(),
+                    packet.getData(),
+                    packet.getDistance()
+            );
+        }
+    }
 }
