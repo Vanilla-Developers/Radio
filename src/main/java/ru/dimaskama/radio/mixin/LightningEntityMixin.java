41a713ab044ab34071eb725948fa718568956876
Damir00109 <internet00109@gmail.com>
2025-12-19 15:05:49 +0300

    mixin fix and assets

--- src/main/java/ru/dimaskama/radio/mixin/LightningEntityMixin.java
+++ src/main/java/ru/dimaskama/radio/mixin/LightningEntityMixin.java
@@ -1,47 +1,59 @@
 package ru.dimaskama.radio.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.class_1297;
-import net.minecraft.class_1299;
-import net.minecraft.class_1538;
-import net.minecraft.class_1937;
-import net.minecraft.class_2338;
-import net.minecraft.class_2350;
-import net.minecraft.class_2680;
-import net.minecraft.class_2741;
-import net.minecraft.class_3218;
-import net.minecraft.class_3481;
-import net.minecraft.class_2338.class_2339;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LightningRodBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.util.math.BlockPos.Mutable;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import ru.dimaskama.radio.block.ModBlocks;
 import ru.dimaskama.radio.block.RadioBlock;
 
-@Mixin({class_1538.class})
-abstract class LightningEntityMixin extends class_1297 {
-	private LightningEntityMixin(class_1299<?> type, class_1937 world) {
-		super(type, world);
-		throw new AssertionError();
-	}
+@Mixin(LightningEntity.class)
+abstract class LightningEntityMixin extends Entity {
 
-	@Inject(
-		method = {"powerLightningRod"},
-		at = {@At("TAIL")}
-	)
-	private void destroyRadio(CallbackInfo ci, @Local class_2338 pos, @Local class_2680 state) {
-		if (this.method_73183() instanceof class_3218 world) {
-			class_2339 mutable = pos.method_25503();
+    private LightningEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+        throw new AssertionError();
+    }
 
-			while (state.method_26164(class_3481.field_61207) && state.method_61767(class_2741.field_12525, class_2350.field_11036) == class_2350.field_11036) {
-				mutable.method_33098(mutable.method_10264() - 1);
-				state = world.method_8320(mutable);
-			}
+    @Inject(
+            method = "powerLightningRod",
+            at = @At("TAIL")
+    )
+    private void destroyRadio(
+            CallbackInfo ci,
+            @Local BlockPos pos,
+            @Local BlockState state
+    ) {
+        if (this.getWorld() instanceof ServerWorld world) {
 
-			if (state.method_27852(ModBlocks.RADIO)) {
-				RadioBlock.burnRadio(world, mutable);
-			}
-		}
-	}
+            Mutable mutable = pos.mutableCopy();
+
+            while (
+                    state.isOf(Blocks.LIGHTNING_ROD)
+                            && state.get(LightningRodBlock.FACING) == Direction.UP
+            ) {
+                mutable.move(Direction.DOWN);
+                state = world.getBlockState(mutable);
+            }
+
+            if (state.isOf(ModBlocks.RADIO)) {
+                RadioBlock.burnRadio(world, mutable);
+            }
+        }
+    }
 }
--- src/main/java/ru/dimaskama/radio/mixin/ServerWorldMixin.java
+++ src/main/java/ru/dimaskama/radio/mixin/ServerWorldMixin.java
@@ -1,6 +1,6 @@
 package ru.dimaskama.radio.mixin;
 
-import net.minecraft.class_3218;
+import net.minecraft.server.world.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -10,25 +10,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.dimaskama.radio.WorldRadioManager;
 import ru.dimaskama.radio.extend.ServerWorldExtend;
 
-@Mixin({class_3218.class})
+@Mixin({ServerWorld.class})
 abstract class ServerWorldMixin implements ServerWorldExtend {
-	@Unique
-	@Nullable
-	@Nullable
-	private WorldRadioManager radio_radioManager;
 
-	@Inject(
-		method = {"<init>"},
-		at = {@At("TAIL")}
-	)
-	private void initTail(CallbackInfo ci) {
-		class_3218 thisServerWorld = (class_3218)this;
-		this.radio_radioManager = new WorldRadioManager(thisServerWorld);
-	}
+    @Unique
+    @Nullable
+    @Nullable
+    private WorldRadioManager radio_radioManager;
 
-	@Nullable
-	@Override
-	public WorldRadioManager radio_getRadioManager() {
-		return this.radio_radioManager;
-	}
+    @Inject(
+            method = {"<init>"},
+            at = {@At("TAIL")}
+    )
+    private void initTail(CallbackInfo ci) {
+        ServerWorld thisServerWorld = (ServerWorld) this;
+        this.radio_radioManager = new WorldRadioManager(thisServerWorld);
+    }
+
+    @Nullable
+    @Override
+    public WorldRadioManager radio_getRadioManager() {
+        return this.radio_radioManager;
+    }
 }
